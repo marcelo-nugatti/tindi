5ee43054aa9769a5376c3d67af1deda8a6f7bf5b
Marcelo Nuñez <real.nugatti@gmail.com>
2020-03-01 03:33:56 -0500

    tot

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from distutils.core import setup
-setup(
-  name = 'tindiapp',
-  packages = ['tindi'], # this must be the same as the name above
-  version = '1.0.1',
-  description = 'Calculadora para colegios',
-  author = 'Marcelo Nuñez',
-  author_email = 'real.nugatti@gmail.com',
-  url = 'https://github.com/marcelo-nugatti/tindi',
-  download_url = 'https://github.com/marcelo-nugatti/tindi/tarball/1.0.1',
-  keywords = ['calculadora', 'colegios', 'tindi'],
-  classifiers = [],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tindi", # Replace with your own username
+    version="0.0.1",
+    author="Marcelo Nunez",
+    author_email="real.nugatti@gmail.com",
+    description="Paquete calculadora",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcelo-nugatti/tindi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
-
-
